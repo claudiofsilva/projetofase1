432374424736a5532bbc570fedd60a28b569bb80
Claudio Silva <claudio.fransilva@gmail.com>
2014-08-22 14:37:06 -0300

    Alteracoes Rotas

--- index.php
+++ index.php
@@ -1,31 +1,40 @@
+<?php 
+$rota = parse_url('http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
+$path= preg_replace('(^/)','',$rota['path']);
+
+if($path){ 
+	if(file_exists($path.'.php')){ 
+		$pagina = $path.'.php';
+	}else{
+		   $pagina = 'erro.php';
+		   http_response_code(404);		   
+		}
+}else{
+	$pagina = 'home.php';
+}
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
+
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Projeto fase 1</title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/fase1.css" rel="stylesheet">
-    <script src="js/bootstrap.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>	
 </head>
 <body>
 
 <div class="container">
 
     <?php require_once('menu.php'); ?>
+	<?php require_once($pagina); ?>
+	
 
-    <?php
-    if (!$_POST AND !$_GET) {
-        require_once('home.php');
-    } elseif ($_GET) {
-        if (file_exists($_GET['pagina'])) {
-            require_once($_GET['pagina']);
-        } else {
-            require_once('erro.php');
-        }
-    } elseif ($_POST) {
-        ?>
+    <? if($_POST) { ?>
         <div class="panel panel-default">
             <div class="panel-body">
                 Dados enviados com sucesso, abaixo seguem os dados que você enviou
@@ -43,10 +52,8 @@
                 <li class="list-group-item"><strong>Mensagem:</strong> <?php echo $_POST['mensagem']; ?></li>
 
             </ul>
-        </div>
-
-    <?php } ?>
-
+        </div>   
+	<? } ?>
     <?php require_once('footer.php'); ?>
 
 </div>
