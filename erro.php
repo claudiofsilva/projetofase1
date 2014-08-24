00cbda6e7d2e4531341a7f8551ef2f2dbb29710d
Claudio Silva <claudio.fransilva@gmail.com>
2014-08-24 13:53:13 -0300

    correções conflitos

--- erro.php
+++ erro.php
@@ -1,5 +1,5 @@
 <div class="jumbotron">
-    <h1>Erro</h1>
+    <h1>Erro 404</h1>
     <p class="lead">A pagina que voce tentou acessar nao existe</p>
-    <p><a class="btn btn-lg btn-success" href="#" role="button">Erro</a></p>
+    <p><a class="btn btn-lg btn-success" href="#" role="button">Erro 404</a></p>
 </div>
--- menu.php
+++ menu.php
@@ -1,10 +1,10 @@
 <div class="masthead">
     <h3 class="text-muted">Projeto fase 1</h3>
     <ul class="nav nav-justified">
-        <li class="active"><a href="?pagina=home.php">Home</a></li>
-        <li><a href="?pagina=empresa.php">Empresa</a></li>
-        <li><a href="?pagina=produtos.php">Produtos</a></li>
-        <li><a href="?pagina=servicos.php">Serviços</a></li>
-        <li><a href="?pagina=contato.php">Contato</a></li>
+        <li class="active"><a href="home">Home</a></li>
+        <li><a href="empresa">Empresa</a></li>
+        <li><a href="produtos">Produtos</a></li>
+        <li><a href="servicos">Serviços</a></li>
+        <li><a href="contato">Contato</a></li>
     </ul>
 </div>
